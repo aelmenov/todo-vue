b5a5bc09630c15945a1892f4654aa31e59f6b003
a.elmenov <a.elmenov@okko.tv>
2023-04-13 11:40:50 +0300

    Add filter

--- src/views/task-list-view/TaskListView.vue
+++ src/views/task-list-view/TaskListView.vue
@@ -1,25 +1,87 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useAppStore } from '@/stores/app';
 import { useRouter } from 'vue-router';
 import Checkbox from 'primevue/checkbox';
 import Button from 'primevue/button';
 import TaskName from './components/TaskName.vue';
 import { RouteName } from '@/router';
+import Toolbar from 'primevue/toolbar';
 
 const store = useAppStore();
 const router = useRouter();
 const userId = Number(router.currentRoute.value.params.userId);
 
-const taskList = computed(() => store.getTasksByUserId(Number(userId)));
+const enum Filter {
+  My,
+  Authored,
+  Assigned,
+}
+
+const filteredTasks = ref(store.getTasksByUserId(userId));
+const filter = ref(Filter.My);
 
 const handleCreateTask = () => router.push({ name: RouteName.TaskCreate, params: { userId } });
+
+const handleMyFilter = () => {
+  filter.value = Filter.My;
+  filteredTasks.value = store.getTasksByUserId(userId);
+}
+
+const handleAuthoredFilter = () => {
+  filter.value = Filter.Authored;
+  filteredTasks.value = store.getTasksByAuthorId(userId);
+}
+
+const handleAssigneedFilter = () => {
+  filter.value = Filter.Assigned;
+  filteredTasks.value = store.getTasksByAssigneeId(userId);
+}
 </script>
 
 <template>
+  <Toolbar class="mb-5">
+    <template #start>
+      <Button
+        label="New"
+        icon="pi pi-plus"
+        size="small"
+        @click="handleCreateTask"
+      />
+
+      <i class="pi pi-bars p-toolbar-separator mr-2" />
+
+      <span class="p-buttonset">
+        <Button
+          label="My Tasks"
+          size="small"
+          :disabled="filter === Filter.My"
+          severity="secondary"
+          @click="handleMyFilter"
+        />
+
+        <Button
+          label="Created by Me"
+          size="small"
+          severity="secondary"
+          :disabled="filter === Filter.Authored"
+          @click="handleAuthoredFilter"
+        />
+
+        <Button
+          label="Assigned to Me"
+          size="small"
+          severity="secondary"
+          :disabled="filter === Filter.Assigned"
+          @click="handleAssigneedFilter"
+        />
+      </span>
+    </template>
+  </Toolbar>
+
   <ul class="mb-5">
     <li
-      v-for="task in taskList"
+      v-for="task in filteredTasks"
       :key="task.id"
       class="flex items-center mb-2"
       :class="{ 'text-gray-400': task.resolved }"
@@ -37,11 +99,4 @@ const handleCreateTask = () => router.push({ name: RouteName.TaskCreate, params:
       />
     </li>
   </ul>
-
-  <Button
-    label="Create Task"
-    icon="pi pi-plus"
-    size="small"
-    @click="handleCreateTask"
-  />
 </template>
